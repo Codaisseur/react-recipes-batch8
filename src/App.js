7af5d235f6c371127a26aba0fe097c976ed53f14
Wouter de Vos <wouter@codaisseur.com>
2017-06-15 19:29:07 +0200

    Add MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import RecipesContainer from './recipes/RecipesContainer'
 import Loading from './components/Loading'
 import LoadErrorMessage from './components/LoadErrorMessage'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './styles/theme'
 
 import './App.css'
 
 class App extends Component {
+  static childContextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext() {
+    return { muiTheme }
+  }
+
   render() {
     return (
-      <div className="App">
-        <LoadErrorMessage />
-        <Loading />
-        <RecipesContainer />
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className="App">
+          <LoadErrorMessage />
+          <Loading />
+          <RecipesContainer />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
